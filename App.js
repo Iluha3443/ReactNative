f28e40093c1b38e8d7cfd8cf28db4e92600ce7c6
Iluha3443 <tkacenkoila01@gmail.com>
2023-09-01 23:03:02 +0300

    task

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigation } from '@react-navigation/stack';
+import { RegistrationScreen } from './Screens/RegistrationScreen/RegistrationScreen';
+import { PostsScreen } from './PostsScreen';
 
-export default function App() {
+const Stack = createStackNavigation();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={RegistrationScreen} />
+        <Stack.Screen name="Posts" component={PostsScreen} />
+        {/* Добавьте другие экраны, если необходимо */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ PostsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export const PostsScreen = () => {
+  return (
+    <View>
+      <Text>Экран постов</Text>
+    </View>
+  );
+}
