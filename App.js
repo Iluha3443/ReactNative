d1e6064fd360cefbdadf285259260fa13d7adec7
Iluha3443 <tkacenkoila01@gmail.com>
2023-09-04 21:49:28 +0300

    task

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigation } from '@react-navigation/stack';
-import { RegistrationScreen } from './Screens/RegistrationScreen/RegistrationScreen';
-import { PostsScreen } from './PostsScreen';
+import { StyleSheet, Text, View } from 'react-native';
+import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen.jsx"
 
-const Stack = createStackNavigation();
+
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={RegistrationScreen} />
-        <Stack.Screen name="Posts" component={PostsScreen} />
-        {/* Добавьте другие экраны, если необходимо */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <>
+      <RegistrationScreen/>
+    </>
+  
+  )
 }
 
+// const styles = StyleSheet.create({
+
+// })
 export default App;
--- Screens/RegistrationScreen/RegistrationScreen.jsx
+++ Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -1,7 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import BackgroundImg from "./bgImage.jpg"
 
 
-// export const RegistrationScreen = () => {
-//     return (
-        
-//     )
-// }
+export const RegistrationScreen = () => {
+    return (
+         <ImageBackground source={BackgroundImg} style={styles.container}>
+          <View >
+       <Text>nkjkjjkn</Text>
+            </View>
+            </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+    
+ }
+})
